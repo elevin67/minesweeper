7ac36933d9c6801f8d8329882b6dae8610ad44f6
Ethan Levin <elevin@macalester.edu>
2017-04-17 22:47:24 -0500

    RandomSolver
    Completed random solver logic. Doesn’t work well without always
    starting on an empty tile.

--- TestSolver.java
+++ TestSolver.java
@@ -3,21 +3,42 @@ package minesweeper;
 import java.util.*;
 
 public class TestSolver {
-  public static void main(String[] args) {
-    Board board = new Board(16,16,40);
-    int[][] i = {{-3,-3,-3},{-2,1,-3},{-3,-3,-3}};
-    int[][] j = {{0,0,0},{-1,1,0},{0,0,0}};
-    int[][] b = board.getBoard();
-    board.printBoard(b);
-    Solver solver = new Solver();
-    int[][] cover = solver.getCover();
-    int[][] randomBoard = solver.pickRandom(b, cover);
-    if(randomBoard==null) {
-      System.out.println("You lose!");
-    } else {
-      board.printBoard(randomBoard);
-    }
-    int[][] k = solver.iterateTile(1,1,j,i);
-    board.printBoard(k);
+
+  public TestSolver() {
+
   }
+
+  public void runOneRandom() {
+    Board board = new Board(16,16,40);
+    Solver solver = new Solver(board);
+    Boolean result = solver.solveBoard(solver.getGameBoard(),solver.getCover());
+    if(result) {
+      System.out.println("you win!");
+    } else {
+      System.out.println("you lost");
+    }
+  }
+
+  public static void main(String[] args) {
+
+  //   int[][] i = {{1,1,-1},{-1,2,1},{2,1,1}};
+  //   int[][] j = {{-3,-3,-2},{-3,2,-3},{-3,-3,-3}};
+  //   int[][] b = board.getBoard();
+  //   board.printBoard(b);
+  //   Solver solver = new Solver();
+  //   int[][] cover = solver.getCover();
+  //   int[][] randomBoard = solver.pickRandom(b, cover);
+  //   if(randomBoard==null) {
+  //     System.out.println("You lose!");
+  //   } else {
+  //     board.printBoard(randomBoard);
+  //   }
+  //   board.printBoard(i);
+  //   board.printBoard(j);
+  //   int[][] k = solver.iterateRandomTile(1,1,i,j);
+  //   board.printBoard(k);
+    TestSolver testSolver = new TestSolver();
+    testSolver.runOneRandom();
+  }
+
 }
